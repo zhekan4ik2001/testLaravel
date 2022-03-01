d5c346ef1315c80ed19e61c5ca11aab8ab0abca1
zhekan4ik2001 <zh.plastinin2015@gmail.com>
2022-03-01 20:52:49 +0300

    Table sorting

---
+++ resources/views/sorting.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Sorting</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>    
+    <div class="container">
+      <div class="row">
+       <div class="panel panel-default">
+        <div class="panel-heading">
+           <h3>Фиксированная шапка таблицы</h3>
+        </div>
+        <table class="table table-fixed">
+           <thead>
+               <tr>
+               <th class="col-xs-2">№</th><th class="col-xs-8">Имя</th><th class="col-xs-2">Лет</th>
+               </tr>
+           </thead>
+           <tbody>
+               <tr>
+                 <td class="col-xs-2">1</td><td class="col-xs-8">Вася</td><td class="col-xs-2">33</td>
+               </tr>
+               <tr>
+                 <td class="col-xs-2">2</td><td class="col-xs-8">Люба</td><td class="col-xs-2">34</td>
+               </tr>
+               <tr>
+                 <td class="col-xs-2">3</td><td class="col-xs-8">Петя</td><td class="col-xs-2">42</td>
+               </tr>
+               <tr>
+                 <td class="col-xs-2">4</td><td class="col-xs-8">Алексей</td><td class="col-xs-2">32</td>
+               </tr>
+               <tr>
+                 <td class="col-xs-2">5</td><td class="col-xs-8">Женя</td><td class="col-xs-2">31</td>
+               </tr>
+               <tr>
+                 <td class="col-xs-2">6</td><td class="col-xs-8">Сергей</td><td class="col-xs-2">36</td>
+               </tr>
+               <tr>
+                 <td class="col-xs-2">7</td><td class="col-xs-8">Андрей</td><td class="col-xs-2">22</td>
+               </tr>
+               <tr>
+                 <td class="col-xs-2">8</td><td class="col-xs-8">Иринка</td><td class="col-xs-2">43</td>
+               </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
